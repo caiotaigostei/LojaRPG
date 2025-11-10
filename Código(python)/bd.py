d49079f5bb4133dd6555ecfb8c7cc743b2ebaff4
ArturLima33 <169162600+ArturLima33@users.noreply.github.com>
2025-11-10 18:47:24 -0300

    Fix database name and enhance connection checks
    Update database name to 'Loja_RPG' and improve connection handling.

--- Código(python)/bd.py
+++ Código(python)/bd.py
@@ -1,23 +1,14 @@
 import mysql.connector
 
 def conectar():
-
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
         password="", #colocar a mesma senha do MySQL aqui.
-        database="loja_rpg"
+        database="Loja_RPG"
     )
+    return conn
 
-    if conn.is_connected():
-        print("Conexão bem sucedida!")
-        return conn
-    
-    else:
-        print("Falha na conexão.")
-        return None
-    
 def desconectar(conn):
-    if conn.is_connected():
+    if conn and conn.is_connected():
         conn.close()
-        print("Conexão encerrada.")
